42dad8fb6fa358901dcc50d9eaa1f87d9294c367
GABAN-A <AREFGABAN1991@GMAIL.COM>
2023-01-15 20:02:30 +0200

    A2

--- nimer/src/components/sidebar.jsx
+++ nimer/src/components/sidebar.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
+import { useAuth } from "./context/auth"
+
+
 
 const Sidebar2 = () => {
+   
 
+    const { user } = useAuth();
+   
+      
         return (
             <div className="row text-center bg-white h-100 ">
-            <div className="col-6 btt col-lg-12 col-md-12   btn btn-light p-1 m-0 "> <NavLink to="/register">
-                  <img className="h-100 ar  w-100"src={require('../images/11.png')} /></NavLink></div>
+                 {((user?.worker===false)?(
+            <div className="col-6 btt col-lg-12 col-md-12   btn btn-light p-1 m-0 "> <NavLink to="/creatorder">
+                  <img className="h-100 ar  w-100"src={require('../images/11.png')} /></NavLink></div>):(<div className="col-6 btt col-lg-12 col-md-12   btn btn-light p-1 m-0 "> <NavLink to="/register">
+                  <img className="h-100 ar  w-100"src={require('../images/11.png')} /></NavLink></div>))}
             <div className="col-6 btt col-lg-12 col-md-12  btn btn-light p-1 m-0"><NavLink to="/menu1"><img className="h-100 ar  w-100"src={require('../images/112.png')} /></NavLink></div>
             <div className="col-6 btt col-lg-12 col-md-12   btn btn-light p-1 m-0">  <a href="https://goo.gl/maps/4ciyLfuC2ZCnMaL27" target="_blank">
                        <img className="h-100 ar w-100"src={require('../images/114.png')} /></a></div> 
